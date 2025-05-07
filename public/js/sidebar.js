ec4075856a9cafa51846b50c67a6ac330ea7ea8a
Aniket Gupta Tech <aniketgupta7219@gmail.com>
2025-05-07 20:27:46 +0530

    all done

---
+++ public/js/sidebar.js
@@ -0,0 +1,8 @@
+// for Sidebar menu
+document.querySelector('#btn').onclick = function () {
+  document.querySelector('.sidebar').classList.toggle('active');
+};
+
+document.querySelector('#btnhide').onclick = function () {
+  document.querySelector('.sidebar').classList.toggle('active');
+};
